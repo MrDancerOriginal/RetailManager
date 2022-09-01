51d8a68dd9177f46ced7171a510e48924e3ed728
MrDancer <40193041+MrDancerOriginal@users.noreply.github.com>
2022-09-01 23:53:37 +0300

    Wire Up the WPF Login form to the API
    Connects the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed

---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public string UserName { get => _userName; set {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }  
+        }
+        public string Password { get => _password; set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get{
+                return UserName?.Length > 0 && Password?.Length > 0;
+            }
+        }
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
