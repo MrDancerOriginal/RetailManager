86aa1887f13aee0073b8c5b0c0108b375498067b
MrDancer <40193041+MrDancerOriginal@users.noreply.github.com>
2022-10-12 23:45:45 +0300

    Get Add To Cart Working
    Adds the func to add items to the cart, get the subtotal, and update the quantilies

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -40,9 +40,22 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set { 
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set { 
@@ -52,7 +65,7 @@ namespace TRMDesktopUI.ViewModels
         }
 
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -60,13 +73,17 @@ namespace TRMDesktopUI.ViewModels
             set { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
         public string SubTotal
         {
             get 
             {
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                    subTotal += item.Product.RetailPrice * item.QuantityInCart;
+                return subTotal.ToString("C");
             }
         }
         public string Tax
@@ -89,15 +106,34 @@ namespace TRMDesktopUI.ViewModels
         {
             get
             {
-                //Make sure something is selected to add
-                //return ItemQuantity?.Length > 0 && Products?.ToArray().Length > 0;
-                return false;
+                return ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity;
             }
         }
+
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                //Hack - There should be a better way of refreshing this
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }  
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity,
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
+
         public bool CanRemoveFromCart
         {
             get
@@ -108,7 +144,7 @@ namespace TRMDesktopUI.ViewModels
         }
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
