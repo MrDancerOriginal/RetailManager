766ea987c0d0017e89d247e76227d72362bbf92f
MrDancer <40193041+MrDancerOriginal@users.noreply.github.com>
2022-10-17 17:30:54 +0300

    Add Saving the Cart to the Database
    All the changes necessary to save the shopping cart data to the db.

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,7 +15,10 @@ namespace TRMDataManager.Controllers
     {
         public void Post(SaleModel sale)
         {
-            
+            SaleData data = new SaleData();
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            data.SaveSale(sale, userId);
         }
 
         //public List<ProductModel> Get()
